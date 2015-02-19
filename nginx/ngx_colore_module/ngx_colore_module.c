bb59a498daa398eef41dee8d8dd1cae4ae37772a
Joe Blackman <j.blackman@ifad.org>
2015-02-19 11:31:55 +0100

    cb

--- nginx/ngx_colore_module/ngx_colore_module.c
+++ nginx/ngx_colore_module/ngx_colore_module.c
@@ -7,10 +7,10 @@
 static ngx_int_t ngx_colore_set_subdirectory(ngx_http_request_t *r, ngx_str_t *res, ngx_http_variable_value_t *v);
 
 static ndk_set_var_t  ngx_colore_set_subdirectory_filter = {
-    NDK_SET_VAR_MULTI_VALUE,
-    (void*) ngx_colore_set_subdirectory,
-    2,
-    NULL
+	NDK_SET_VAR_MULTI_VALUE,
+	(void*) ngx_colore_set_subdirectory,
+	2,
+	NULL
 };
 
 static ngx_command_t ngx_colore_commands[] = {
@@ -63,26 +63,26 @@ static ngx_int_t ngx_colore_set_subdirectory
 	ngx_http_variable_value_t *p_length;
 	ngx_int_t                  length = 0;
 	u_char                     p[COLORE_MD5_HEX_LENGTH+1] = {0};
-    u_char                     *subdir;
-    ngx_md5_t                  md5;
-    u_char                     md5_buf[MD5_DIGEST_LENGTH];
+	u_char                     *subdir;
+	ngx_md5_t                  md5;
+	u_char                     md5_buf[MD5_DIGEST_LENGTH];
 
 	/* get length */
 
 	p_length = v + 1;
 	length = ngx_atoi(p_length->data, p_length->len);
 	if( length <= 0 || length > COLORE_MD5_HEX_LENGTH) {
-       	ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "set_colore_subdir: bad \"length\" argument: %v", p_length );
-       	return NGX_ERROR;
+		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "set_colore_subdir: bad \"length\" argument: %v", p_length );
+		return NGX_ERROR;
 	}
 
 	/* construct MD5 */
 
-    ngx_md5_init(&md5);
-    ngx_md5_update(&md5, v->data, v->len);
-    ngx_md5_final(md5_buf, &md5);
+	ngx_md5_init(&md5);
+	ngx_md5_update(&md5, v->data, v->len);
+	ngx_md5_final(md5_buf, &md5);
 
-    ngx_hex_dump(p, md5_buf, sizeof(md5_buf));
+	ngx_hex_dump(p, md5_buf, sizeof(md5_buf));
 
 	/* take substring */
 
@@ -91,8 +91,8 @@ static ngx_int_t ngx_colore_set_subdirectory
 
 	/* return */
 
-    res->data = subdir;
-    res->len = length;
+	res->data = subdir;
+	res->len = length;
 	return NGX_OK;
 }
 
